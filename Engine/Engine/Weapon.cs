cae966e388e53c2d54ef9808cb05a74aec514f76
Jake Coltman <jakecoltman@sky.com>
2015-10-04 20:58:25 +0100

    Added basic implementation of weapon and a weapon factory

--- Engine/Engine/Weapon.cs
+++ Engine/Engine/Weapon.cs
@@ -16,7 +16,41 @@ namespace Engine
         int get_power();
     }
 
-    class Weapon
+    class WeaponFactory
     {
+        IWeapon create(WeaponType type, int weight, int power)
+        {
+            return new Weapon(type, weight, power);
+        }
+    }
+
+    class Weapon : IWeapon
+    {
+
+        int power;
+        int weight;
+        WeaponType type;
+
+        public Weapon(WeaponType type, int power, int weight)
+        {
+            this.power = power;
+            this.weight = weight;
+            this.type = type;
+        }
+
+        public int get_power()
+        {
+            return power;
+        }
+
+        public WeaponType get_type()
+        {
+            return type;
+        }
+
+        public int get_weight()
+        {
+            return weight;
+        }
     }
 }
