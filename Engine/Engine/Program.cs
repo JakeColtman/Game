ad933976c64b6eb49734fc670ed497bc5e765afb
Jake Coltman <jakecoltman@sky.com>
2015-10-04 20:49:10 +0100

    Added weapon type enum and weapon interface

--- Engine/Engine/Program.cs
+++ Engine/Engine/Program.cs
@@ -10,6 +10,8 @@ namespace Engine
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
---
+++ Engine/Engine/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+
+    enum WeaponType { Sword, Axe, Lance }
+
+    interface IWeapon
+    {
+        WeaponType get_type();
+        int get_weight();
+        int get_power();
+    }
+
+    class Weapon
+    {
+    }
+}
