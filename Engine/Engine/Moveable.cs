9581d3bdbc1c7a9e07177972cf768fdb976370de
Jake Coltman <=>
2015-10-20 09:10:16 +0100

    Created a movement handler to implement the IMoveable interface for units.  This will be added onto items to give them the ability to move

--- Engine/Engine/Moveable.cs
+++ Engine/Engine/Moveable.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Engine.Mappings;
 
 namespace Engine
 {
@@ -13,10 +14,36 @@ namespace Engine
         bool add_movement_subscriber(Mappings.IMapUpdatable map);
     }
 
-    public class MovementHandler
+    public class MovementHandler : IMoveable
     {
         IList<Mappings.IMapUpdatable> subscribers;
+        Mappings.ICoordinate current_pos;
 
+        public MovementHandler(Mappings.ICoordinate pos)
+        {
+            current_pos = pos;
+            subscribers = new List<Mappings.IMapUpdatable>();
+        }
+
+        public bool add_movement_subscriber(IMapUpdatable map)
+        {
+            try {
+                subscribers.Add(map);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public bool try_and_move(ICoordinate end_coord)
+        {
+            if (subscribers.All(x => x.can_move(current_pos, end_coord)))
+                return subscribers.Select(x => x.move(current_pos, end_coord)).All(x => x);
+            else return false;       
+        }
     }
 
 
