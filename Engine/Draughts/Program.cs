13baca2eba0870b31131fc023ea2098df825720a
Jake Coltman <jakecoltman@sky.com>
2015-10-25 17:47:03 +0000

    Added basic unit and factory to draughs implementation

--- Engine/Draughts/Program.cs
+++ Engine/Draughts/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Engine;
 using Engine.Mappings.Coordinates;
+using Engine.Player;
+using Engine.Unit;
 
 namespace Draughts
 {
@@ -12,16 +14,27 @@ namespace Draughts
     class Man
     {
         ICoordinate<Iso2D> _pos;
+        TwoPlayers _side;
 
-        public Man(ICoordinate<Iso2D> position)
+        public Man(ICoordinate<Iso2D> position, TwoPlayers side)
         {
             _pos = position;
+            _side = side;
         }
     }
 
-    class ManFacotry : IUnitFactory<>
+    struct ManCreationInstruction
     {
+        public ICoordinate<Iso2D> position;
+        public TwoPlayers side;
+    }
 
+    class ManFacotry : IUnitFactory<ManCreationInstruction, Man>
+    {
+        public Man create(ManCreationInstruction instruction)
+        {
+            return new Man(instruction.position, instruction.side);
+        }
     }
 
     class Program
