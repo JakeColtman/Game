3b8009786c0061f0d4aff65d0fe2121965b48726
Jake Coltman <=>
2015-10-27 07:06:12 +0000

    Added and and or composition handlers to allow more complex handlers to be build by composition of objects

---
+++ Engine/Engine/Rules/Composition/And.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Rules.Composition
+{
+    class And : IMessageHandler
+    {
+
+        IMessageHandler _left;
+        IMessageHandler _right;
+
+        public And(IMessageHandler left, IMessageHandler right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public bool can_handle(Message message)
+        {
+            return _left.can_handle(message) && _right.can_handle(message);
+        }
+
+        public bool process(Message message)
+        {
+            return _left.process(message) && _right.process(message);
+        }
+
+        public bool will_allow(Message message)
+        {
+            return _left.will_allow(message) && _right.will_allow(message);
+        }
+    }
+}
