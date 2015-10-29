155833b0e29c306e73bd05939cc2f5abad5371c4
Jake Coltman <jakecoltman@sky.com>
2015-10-29 08:00:25 +0000

    Added isometric directions

---
+++ Engine/Engine/Geometry/Directions/Default/IsometricDirections.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Geometry.Directions.Default
+{
+    class UpLeft : Direction
+    {
+    }
+    class UpRight : Direction
+    {
+    }
+    class DownLeft : Direction
+    {
+    }
+    class DownRight : Direction
+    {
+    }
+}
