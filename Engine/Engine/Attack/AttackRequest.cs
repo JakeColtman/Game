1a71e9ee84b11bd1ac23f75be163dc4e9804a2d7
Jake Coltman <jakecoltman@sky.com>
2015-10-29 08:00:56 +0000

    Removed unnesc old coordinates code

---
+++ Engine/Engine/Attack/AttackRequest.cs
@@ -0,0 +1,15 @@
+﻿using Engine.Unit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Attack
+{
+    public class AttackRequest<T> : Message
+    {
+        public IEntity<T> attacker;
+        public IEntity<T> attacked;
+    }
+}
