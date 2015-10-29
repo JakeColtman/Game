b8a45f8d87d20fa99603284a5491fabecdfb5a01
Jake Coltman <jakecoltman@sky.com>
2015-10-29 09:02:52 +0000

    Implemented interface change

--- Engine/Engine/Maps/Default/IsometricMap.cs
+++ Engine/Engine/Maps/Default/IsometricMap.cs
@@ -11,10 +11,11 @@ namespace Engine.Maps
 {
     class IsometricMap : IMapReader<IPoint>
     {
+
         // Simple infinitely extendable 2D grid of points.  Will provide multiple different objects if asked for the same point 
         //Add some DI here to handle multiple types of coords
 
-        public IPoint get_point_by_coordinate(Coordinate coord)
+        public IPoint get_from_coordinate(Coordinate coord)
         {
             if (!(coord is IsometricCood)) throw new NotImplementedException("Only TwoD grid coords are supported by the map");
 
