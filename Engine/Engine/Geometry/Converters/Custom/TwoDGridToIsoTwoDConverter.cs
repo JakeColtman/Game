43b5d55c92f2c59d29e266f787733e4fb7606978
Jake Coltman <jakecoltman@sky.com>
2015-10-31 20:53:33 +0000

    Added two d to iso two d converter

---
+++ Engine/Engine/Geometry/Converters/Custom/TwoDGridToIsoTwoDConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Geometry.Coordinates;
+
+namespace Engine.Geometry.Converters.Custom
+{
+    class TwoDGridToIsoTwoDConverter : IConverter
+    {
+        public Coordinate get_point_from_coordinate(Coordinate coord)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
