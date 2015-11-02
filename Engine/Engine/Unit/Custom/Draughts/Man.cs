1d2e97bed9eb486f3444b1ca4a372393cddbff74
Jake Coltman <jakecoltman@sky.com>
2015-11-02 20:10:55 +0000

    Added a draughts man

---
+++ Engine/Engine/Unit/Custom/Draughts/Man.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Geometry;
+using Engine.Player;
+
+namespace Engine.Unit.Custom.Draughts
+{
+    class Man : IEntity
+    {
+
+        IPoint _pos;
+
+        public Man(IPoint pos)
+        {
+            _pos = pos;
+        }
+
+        public IPoint get_pos()
+        {
+            return _pos;
+        }
+
+        public TwoPlayers get_side()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEntity move_in_direction(Direction direction)
+        {
+            return new Man(_pos.get_next_point_in_direction(direction));
+        }
+    }
+}
