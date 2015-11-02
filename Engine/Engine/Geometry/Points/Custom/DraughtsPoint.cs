31a7bdd16af49cdd282b5255a7a2f1a33213d399
Jake Coltman <jakecoltman@sky.com>
2015-11-02 20:04:13 +0000

    Implemented draughtspoint

--- Engine/Engine/Geometry/Points/Custom/DraughtsPoint.cs
+++ Engine/Engine/Geometry/Points/Custom/DraughtsPoint.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine.Geometry.Directions.Custom.Draughts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,25 @@ namespace Engine.Geometry.Points.Custom
 {
     class DraughtsPoint : IPoint
     {
+        
+        int _left;
+        int _right;
+
+        public DraughtsPoint(int left, int right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+
         public IPoint get_next_point_in_direction(Direction direction)
         {
-            throw new NotImplementedException();
+            if (direction is WhiteManLeftDirection) return new IsometricPoint(_left + 1, _right);
+            if (direction is WhiteManRightDirection) return new IsometricPoint(_left, _right + 1);
+            if (direction is BlackManLeftDirection) return new IsometricPoint(_left - 1, _right);
+            if (direction is BlackManRightDirection) return new IsometricPoint(_left, _right - 1);
+
+            throw new NotImplementedException(direction.ToString() + " is not a valid dimension");
         }
     }
 }
