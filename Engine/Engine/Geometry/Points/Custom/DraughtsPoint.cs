58d0e7adfdf49c96ea3f1e47181edbf263c70bdb
Jake Coltman <jakecoltman@sky.com>
2015-11-02 19:59:01 +0000

    Added basic draughts point

---
+++ Engine/Engine/Geometry/Points/Custom/DraughtsPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Geometry.Points.Custom
+{
+    class DraughtsPoint : IPoint
+    {
+        public IPoint get_next_point_in_direction(Direction direction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
